bluzphp/module-contact-us

Prepare for framework 7.6.1

application/modules/contact-us/controllers/crud.php
@@ -20,8 +20,7 @@ return function () {
     /**
      * @var Controller $this
      */
-    $crud = new Crud();
-    $crud->setCrud(ContactUs\Crud::getInstance());
+    $crud = new Crud(ContactUs\Crud::getInstance());
 
     $crud->get('system', 'crud/get');
     $crud->post('system', 'crud/post');

bluzphp/module-categories

application/modules/categories/controllers/crud.php
@@ -42,9 +42,7 @@ return function ($parentId = null, $rootId = null) {
     }
     $this->assign('tree', $tree);
 
-    $crud = new Crud();
-
-    $crud->setCrud(Categories\Crud::getInstance());
+    $crud = new Crud(Categories\Crud::getInstance());
 
     $crud->get('system', 'crud/get');
     $crud->post('system', 'crud/post');